1538e001943d5ff4ad1a99a6f788d5b002dcda58
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-09-25 14:59:52 +0200

    first commit

---
+++ Fightnign/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int hp = 100;
+int MasterSandor = 100;
+while (hp > 0 && MasterSandor > 0)
+{
+    Console.WriteLine($"Hjältens Hp: {hp}");
+    hp -= Random.Shared.Next(14);
+
+    Console.WriteLine($"MasterSandor Hp: {MasterSandor}");  
+    MasterSandor -= Random.Shared.Next(14);
+
+    Console.ReadLine();
+}
+
+Console.ReadLine();
