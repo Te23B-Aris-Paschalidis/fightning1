a159c68b47b3fb92925ddeb8893166530b3b8f59
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-10-01 13:29:02 +0200

    WDAADF

--- Fightnign/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Fightnign/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1538e001943d5ff4ad1a99a6f788d5b002dcda58")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
